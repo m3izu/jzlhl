07e3ae2b6fc98c254423a536952ce2cf960ab137
m3izu <rfsgalendez@gmail.com>
2024-02-13 04:42:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3...  See you after 3PM!! (klase panako)";
+  titleElement.innerHTML = "See you after 3PM!! (klase panako)";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
